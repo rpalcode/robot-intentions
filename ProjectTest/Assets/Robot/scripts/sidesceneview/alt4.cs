161cf67a10282e86cf3248c28e89543f30438bcd
Roger Milton <rogermilton@mail.usf.edu>
2014-03-07 18:43:49 -0500

    fixed bugs in code for alternate camera viewing

--- ProjectTest/Assets/Robot/scripts/sidesceneview/alt4.cs
+++ ProjectTest/Assets/Robot/scripts/sidesceneview/alt4.cs
@@ -2,8 +2,7 @@
 using System.Collections;
 
 public class alt4 : MonoBehaviour {
-    public GameObject c1;
-    public GameObject c2;
+ 
 	// Use this for initialization
 	void Start () {
 	
@@ -11,17 +10,12 @@ public class alt4 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        c1.SetActive(true);
-        c2.SetActive(false);
+     
     if(Input.GetKeyDown("0"))
         {
             Application.LoadLevel(4);
         }
-    if (Input.GetKeyDown("9"))
-    {
-        c1.SetActive(false);
-        c2.SetActive(true);
-    }
+  
 
 	}
 }
--- ProjectTest/Assets/Robot/scripts/sidesceneview/alt5.cs
+++ ProjectTest/Assets/Robot/scripts/sidesceneview/alt5.cs
@@ -2,9 +2,7 @@
 using System.Collections;
 
 public class alt5 : MonoBehaviour {
-    private GameObject c1;
-    private GameObject c2;
-    public int camSwitch;
+  
 	// Use this for initialization
 	void Start () {
 	}
@@ -12,24 +10,11 @@ public class alt5 : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
        // int camSwitch = 0;
-        if (camSwitch == 0)
-        {
-            c1.SetActive(true);
-            c2.SetActive(false);
-        }
+
         if (Input.GetKeyDown("0"))
         {
             Application.LoadLevel(6);
         }
-        if (Input.GetKey("9"))
-        {
-            camSwitch=1;
-        }
-
-        if(camSwitch==1)
-        {
-            c1.SetActive(false);
-            c2.SetActive(true);
-        }
+       
 	}
 }
