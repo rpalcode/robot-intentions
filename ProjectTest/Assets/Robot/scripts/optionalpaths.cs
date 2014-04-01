8738e99e5fa0b4048f2655ca6fa3d5f24f962424
Arrjayyyy4 <rogermilton@mail.usf.edu>
2014-04-01 16:34:26 -0400

    UI text taken out, obstacles shortened, map can now change depending on user input where button on keyboard pressed will cause only one path to show up (q=left path, w=mid path, e=right path, r=all paths)

--- ProjectTest/Assets/Robot/scripts/optionalpaths.cs
+++ ProjectTest/Assets/Robot/scripts/optionalpaths.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 
 public class optionalpaths : MonoBehaviour {
-
+	public GameObject red;
+	public GameObject blue;
+	public GameObject green;
+	public GameObject map;
    /* void OnGUI()
     {
        GUI.Box(new Rect(250, 900, 200, 270), "Please pick a path.\n4.Top(Red)\n5.Mid(Green)\n6.Bottom(Blue)");
@@ -11,11 +14,48 @@ public class optionalpaths : MonoBehaviour {
 	}
 */
 	void Start () {
-        
+		red.SetActive(false);
+		blue.SetActive(false);
+		green.SetActive(false);
+		map.SetActive(true);
 	}
 
 	// Update is called once per frame
 	void Update () {
+
+		//change paths
+
+		if (Input.GetKeyDown("q"))
+		{
+			red.SetActive(true);
+			blue.SetActive(false);
+			green.SetActive(false);
+			map.SetActive(false);
+		}
+		if (Input.GetKeyDown("w"))
+		{
+			red.SetActive(false);
+			blue.SetActive(false);
+			green.SetActive(true);
+			map.SetActive(false);
+		}
+		if (Input.GetKeyUp("e"))
+		{
+			red.SetActive(false);
+			green.SetActive(false);
+			blue.SetActive(true);
+			map.SetActive(false);
+		}
+		if (Input.GetKeyUp("r"))
+		{
+			red.SetActive(false);
+			green.SetActive(false);
+			blue.SetActive(false);
+			map.SetActive(true);
+		}
+
+		//load levels
+
         if (Input.GetKeyDown("9"))
         {
             Application.LoadLevel(2);
