5b72a9861f1163f128deffb6e5c7ff33f690d6b5
Arrjayyyy4 <rogermilton@mail.usf.edu>
2014-04-06 14:52:23 -0400

    Successfully added file output from left path scenarios, however a new commit will be added soon once extensive testing has been completed on the other paths

--- ProjectTest/Assets/Robot/scripts/scene4.cs
+++ ProjectTest/Assets/Robot/scripts/scene4.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class scene4 : MonoBehaviour {
     public GameObject straight;
@@ -7,6 +8,21 @@ public class scene4 : MonoBehaviour {
     public GameObject right;
     public GameObject robot;
 	public Camera camera01;
+	
+	public System.DateTime date1 = new System.DateTime(1996, 6, 3, 22, 15, 0);
+	public System.DateTime date2 = new System.DateTime(1996, 12, 6, 13, 2, 0);
+	public System.TimeSpan deltaDate1;
+	public System.TimeSpan deltaDate2;
+	public System.TimeSpan deltaDate3;
+	
+	
+	public int stop1=0;
+	public int stop2=0;
+	public int stop3=0;
+	
+	public int pause=0;
+	public string res1, res2, res3;
+
 
     void leftArrow()
     {
@@ -31,27 +47,43 @@ public class scene4 : MonoBehaviour {
 
     private void option1()
     {
-
-		if (Input.GetKeyDown("p"))
-		{
-			Time.timeScale =0;
-			camera01.enabled = false;
-		}
-		if (Input.GetKeyDown("o"))
-		{
-			camera01.enabled = true;
-			
-			Time.timeScale =1;
-		}
-
-		Debug.Log(Time.timeSinceLevelLoad);
+		UnityEngine.Debug.Log("LEVEL = " + Application.loadedLevel);
 
 		transform.Translate(0, -5 * Time.deltaTime, 0);
         upArrow();
+		
+		if ((Time.timeSinceLevelLoad > 1.5 && Time.timeSinceLevelLoad < 1.6) && stop1==0)
+		{
+			date1 = System.DateTime.Now;
+			pause=1;
+			Time.timeScale =0;
+			camera01.enabled = false;
+			stop1=1;
+		}
+		
+		if ((Time.timeSinceLevelLoad > 12.8 && Time.timeSinceLevelLoad < 12.9) && stop2==0)
+		{
+			date1 = System.DateTime.Now;
+			pause=1;
+			Time.timeScale =0;
+			camera01.enabled = false;
+			stop2=1;
+		}
+		
+		if ((Time.timeSinceLevelLoad > 14.8 && Time.timeSinceLevelLoad < 14.9) && stop3==0)
+		{
+			date1 = System.DateTime.Now;
+			pause=1;
+			Time.timeScale =0;
+			camera01.enabled = false;
+			stop3=1;
+		}
+		//
+
 		if (Time.timeSinceLevelLoad > 2 && Time.timeSinceLevelLoad < 4) { leftArrow(); }
 		if (Time.timeSinceLevelLoad > 4 && Time.timeSinceLevelLoad > 8) { upArrow(); }
 		if (Time.timeSinceLevelLoad > 11 && Time.timeSinceLevelLoad < 14) { rightArrow(); }
-		if (Time.timeSinceLevelLoad > 13 && Time.timeSinceLevelLoad < 17) { rightArrow(); }
+		if (Time.timeSinceLevelLoad > 13 && Time.timeSinceLevelLoad < 16.5) { rightArrow(); }
 
 		if (Time.timeSinceLevelLoad > 3 && Time.timeSinceLevelLoad < 4)
         {
@@ -67,10 +99,37 @@ public class scene4 : MonoBehaviour {
         {
             transform.Rotate(0, 0, 45 * Time.deltaTime);
             transform.Translate(0, -5 * Time.deltaTime, 0);
+
         }
 
-		if (Time.timeSinceLevelLoad >= 18.5)
-        {
+		
+		if (Time.timeSinceLevelLoad >= 16.5)
+		{
+
+			if(Application.loadedLevel == 0)
+			{
+				using (StreamWriter writer = new StreamWriter("leftpatharrowchest.txt"))
+				{
+					writer.WriteLine("short-term view with arrows on screen");
+					writer.WriteLine(res1 + " " + deltaDate1);
+					writer.WriteLine(res2 + " " + deltaDate2);
+					writer.WriteLine(res3 + " " + deltaDate3);
+					writer.WriteLine(" ");
+				}
+			}
+
+			if(Application.loadedLevel == 1)
+			{
+				using (StreamWriter writer = new StreamWriter("leftpatharrowfloor.txt"))
+				{
+					writer.WriteLine("short-term view with arrows on floor");
+					writer.WriteLine(res1 + " " + deltaDate1);
+					writer.WriteLine(res2 + " " + deltaDate2);
+					writer.WriteLine(res3 + " " + deltaDate3);
+					writer.WriteLine(" ");
+				}
+			}
+
             Application.LoadLevel(2);
         }        
     }
@@ -79,7 +138,146 @@ public class scene4 : MonoBehaviour {
 	// Use this for initialization
 
 	void Start () {
-        //counter = 0;
+		Time.timeScale=1;
+	}
+	
+	//
+	void OnGUI()
+	{
+		if(pause==1)
+		{
+			int choice=0;
+			
+			GUI.Box(new Rect(150,350,300,100), "Which way is the robot going to go?\n(From robot's point of view)");
+			
+			if(GUI.Button(new Rect(150,400,100,50), "left")) 
+			{
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					res1 = "left";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					res2 = "left";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					res3 = "left";
+				}
+				choice=1;
+				
+			}
+			if(GUI.Button(new Rect(250,400,100,50), "forward")) 
+			{
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					res1 = "forward";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					res2 = "forward";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					res3 = "forward";
+				}
+				choice=1;
+			}
+			
+			if(GUI.Button(new Rect(350,400,100,50), "right")) 
+			{
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					res1 = "right";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					res2 = "right";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					res3 = "right";
+				}
+				choice=1;
+			}
+			/*
+			GUI.Box(new Rect(150+500,350,300,100), "Which way is the robot going to go?\n(From robot's point of view)");
+			
+			if(GUI.Button(new Rect(150+500,400,100,50), "left")) 
+			{
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					res1 = "left";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					res2 = "left";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					res3 = "left";
+				}
+				choice=1;
+
+			}
+			if(GUI.Button(new Rect(250+500,400,100,50), "forward")) 
+			{
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					res1 = "forward";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					res2 = "forward";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					res3 = "forward";
+				}
+				choice=1;
+			}
+			
+			if(GUI.Button(new Rect(350+500,400,100,50), "right")) 
+			{
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					res1 = "right";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					res2 = "right";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					res3 = "right";
+				}
+				choice=1;
+			}
+*/
+			if(choice==1)
+			{
+				date2 = System.DateTime.Now;
+				UnityEngine.Debug.Log ("ELAPSED = " + (date2-date1));
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					deltaDate1 = (date2-date1);
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					deltaDate2 = (date2-date1);
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					deltaDate3 = (date2-date1);
+				}
+				camera01.enabled = true;
+				Time.timeScale =1;
+				pause=0;
+				option1 ();
+				
+			}
+			
+		}
 	}
 
 	// Update is called once per frame
--- ProjectTest/Assets/Robot/scripts/scene4b.cs
+++ ProjectTest/Assets/Robot/scripts/scene4b.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Diagnostics;
-using System.Threading;
-
+//
+//using System.Diagnostics;
+//using System.Threading;
+using System.IO;
+//
 
 public class scene4b : MonoBehaviour {
     public GameObject straight;
@@ -16,15 +18,23 @@ public class scene4b : MonoBehaviour {
 	public GameObject three;
 	public GameObject four;
 
+	//
+
 	public System.DateTime date1 = new System.DateTime(1996, 6, 3, 22, 15, 0);
 	public System.DateTime date2 = new System.DateTime(1996, 12, 6, 13, 2, 0);
+	public System.TimeSpan deltaDate1;
+	public System.TimeSpan deltaDate2;
+	public System.TimeSpan deltaDate3;
 
+	
 	public int stop1=0;
 	public int stop2=0;
 	public int stop3=0;
 
-	public static int pause=0;
+	public int pause=0;
+	public string res1, res2, res3;
 
+	//
 
 	void StartSide()
 	{
@@ -44,15 +54,10 @@ public class scene4b : MonoBehaviour {
 
 		StartSide ();
 
-		if (Input.GetKeyDown("p"))
-		{
-			date1 = System.DateTime.Now;
-			pause=1;
-			Time.timeScale =0;
-			camera01.enabled = false;
-		}
+		//
+		UnityEngine.Debug.Log("LEVEL = " + Application.loadedLevel);
 
-		if ((Time.timeSinceLevelLoad > 1 && Time.timeSinceLevelLoad < 1.1) && stop1==0)
+		if ((Time.timeSinceLevelLoad > 1.5 && Time.timeSinceLevelLoad < 1.6) && stop1==0)
 		{
 			date1 = System.DateTime.Now;
 			pause=1;
@@ -78,7 +83,7 @@ public class scene4b : MonoBehaviour {
 			camera01.enabled = false;
 			stop3=1;
 		}
-
+		//
 
 		if (Time.timeSinceLevelLoad > 0 && Time.timeSinceLevelLoad < 3)
 		{
@@ -115,15 +120,8 @@ public class scene4b : MonoBehaviour {
 			three.SetActive(true);
 			four.SetActive(false);
 		}
-		if (Time.timeSinceLevelLoad > 15 && Time.timeSinceLevelLoad < 17)
+		if (Time.timeSinceLevelLoad > 15 && Time.timeSinceLevelLoad < 16.5)
 		{
-			one.SetActive(false);
-			two.SetActive(false);
-			three.SetActive(false);
-			four.SetActive(true);
-		}
-		if (Time.timeSinceLevelLoad > 17 && Time.timeSinceLevelLoad < 18)
-		{			
 			one.SetActive(false);
 			two.SetActive(false);
 			three.SetActive(false);
@@ -142,24 +140,51 @@ public class scene4b : MonoBehaviour {
             transform.Rotate(0, 0, 45 * Time.deltaTime);
             transform.Translate(0, -5 * Time.deltaTime, 0);
         }
-		if (Time.timeSinceLevelLoad > 15 && Time.timeSinceLevelLoad < 17)
+		if (Time.timeSinceLevelLoad > 15 && Time.timeSinceLevelLoad < 16.5)
         {
             transform.Rotate(0, 0, 45 * Time.deltaTime);
             transform.Translate(0, -5 * Time.deltaTime, 0);
         }
 
-		if (Time.timeSinceLevelLoad >= 18)
-        {
-            Application.LoadLevel(2);
-        }        
+		if (Time.timeSinceLevelLoad >= 16.5)
+		{
+
+			if(Application.loadedLevel == 0)
+			{
+				using (StreamWriter writer = new StreamWriter("leftpatharrowchest.txt"))
+				{
+					writer.WriteLine("mid-term view with map on screen");
+					writer.WriteLine(res1 + " " + deltaDate1);
+					writer.WriteLine(res2 + " " + deltaDate2);
+					writer.WriteLine(res3 + " " + deltaDate3);
+					writer.WriteLine(" ");
+				}
+			}
+			
+			if(Application.loadedLevel == 1)
+			{
+				using (StreamWriter writer = new StreamWriter("leftpatharrowfloor.txt"))
+				{
+					writer.WriteLine("mid-term view without map on screen");
+					writer.WriteLine(res1 + " " + deltaDate1);
+					writer.WriteLine(res2 + " " + deltaDate2);
+					writer.WriteLine(res3 + " " + deltaDate3);
+					writer.WriteLine(" ");
+				}
+			}
+			
+			Application.LoadLevel(2);
+		}         
     }
 
 
 	// Use this for initialization
 
 	void Start () {
+		Time.timeScale=1;
 	}
 
+	//
 	void OnGUI()
 	{
 		if(pause==1)
@@ -167,18 +192,55 @@ public class scene4b : MonoBehaviour {
 			int choice=0;
 
 			GUI.Box(new Rect(150,350,300,100), "Which way is the robot going to go?\n(From robot's point of view)");
-
+			
 			if(GUI.Button(new Rect(150,400,100,50), "left")) 
 			{
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					res1 = "left";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					res2 = "left";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					res3 = "left";
+				}
 				choice=1;
+				
 			}
 			if(GUI.Button(new Rect(250,400,100,50), "forward")) 
 			{
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					res1 = "forward";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					res2 = "forward";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					res3 = "forward";
+				}
 				choice=1;
 			}
-	
+			
 			if(GUI.Button(new Rect(350,400,100,50), "right")) 
 			{
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					res1 = "right";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					res2 = "right";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					res3 = "right";
+				}
 				choice=1;
 			}
 			/*
@@ -186,15 +248,52 @@ public class scene4b : MonoBehaviour {
 			
 			if(GUI.Button(new Rect(150+500,400,100,50), "left")) 
 			{
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					res1 = "left";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					res2 = "left";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					res3 = "left";
+				}
 				choice=1;
+
 			}
 			if(GUI.Button(new Rect(250+500,400,100,50), "forward")) 
 			{
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					res1 = "forward";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					res2 = "forward";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					res3 = "forward";
+				}
 				choice=1;
 			}
 			
 			if(GUI.Button(new Rect(350+500,400,100,50), "right")) 
 			{
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					res1 = "right";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					res2 = "right";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					res3 = "right";
+				}
 				choice=1;
 			}
 */
@@ -202,9 +301,18 @@ public class scene4b : MonoBehaviour {
 			{
 				date2 = System.DateTime.Now;
 				UnityEngine.Debug.Log ("ELAPSED = " + (date2-date1));
-
-				//UnityEngine.Debug.Log ("TIME = " + timee);
-
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					deltaDate1 = (date2-date1);
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					deltaDate2 = (date2-date1);
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					deltaDate3 = (date2-date1);
+				}
 				camera01.enabled = true;
 				Time.timeScale =1;
 				pause=0;
@@ -214,6 +322,8 @@ public class scene4b : MonoBehaviour {
 
 		}
 	}
+
+	//
 
 	// Update is called once per frame
 	void Update () {
