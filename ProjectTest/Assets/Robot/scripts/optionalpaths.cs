ca98d550b8e72e0b41d4e7043c16506acc85626b
Arrjayyyy4 <rogermilton@mail.usf.edu>
2014-04-08 23:39:45 -0400

    Added scenes with somewhat of a robot's point of view while enabling the user to be able to see part of the robot

--- ProjectTest/Assets/Robot/scripts/optionalpaths.cs
+++ ProjectTest/Assets/Robot/scripts/optionalpaths.cs
@@ -8,6 +8,7 @@ public class optionalpaths : MonoBehaviour {
 	public GameObject map;
 	public int path=0;
 	public Camera camera01;
+	public int pause;
    
 	void Start () {
 		red.SetActive(false);
@@ -18,8 +19,116 @@ public class optionalpaths : MonoBehaviour {
 	}
 
 	// Update is called once per frame
+
+	void OnGUI()
+	{
+		if(Time.timeSinceLevelLoad>3)
+		{
+			camera01.enabled = false;
+			pause = 1;
+			
+		}
+		if(pause==1)
+		{
+
+			
+			GUI.Box(new Rect(150,350,300,100), "Which way is the robot going to go?\n(From robot's point of view)");
+			
+			if(GUI.Button(new Rect(150,400,100,50), "left")) 
+			{
+				Application.LoadLevel(16);
+				
+			}
+			if(GUI.Button(new Rect(250,400,100,50), "forward")) 
+			{
+				Application.LoadLevel(17);
+
+			}
+			
+			if(GUI.Button(new Rect(350,400,100,50), "right")) 
+			{
+				Application.LoadLevel(18);
+
+			}
+			/*
+			GUI.Box(new Rect(150+500,350,300,100), "Which way is the robot going to go?\n(From robot's point of view)");
+			
+			if(GUI.Button(new Rect(150+500,400,100,50), "left")) 
+			{
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					res1 = "left";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					res2 = "left";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					res3 = "left";
+				}
+				choice=1;
+
+			}
+			if(GUI.Button(new Rect(250+500,400,100,50), "forward")) 
+			{
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					res1 = "forward";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					res2 = "forward";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					res3 = "forward";
+				}
+				choice=1;
+			}
+			
+			if(GUI.Button(new Rect(350+500,400,100,50), "right")) 
+			{
+				if(stop1 == 1 && stop2==0 && stop3==0) 
+				{
+					res1 = "right";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==0)
+				{
+					res2 = "right";
+				}
+				if(stop1 == 1 && stop2==1 && stop3==1)
+				{
+					res3 = "right";
+				}
+				choice=1;
+			}
+*/
+
+				
+
+			
+		}
+	}
+
+
 	void Update () {
+		//rotate camera
+		Debug.Log(Time.timeSinceLevelLoad);
 		//change paths
+		int choice=0;
+		if (Input.GetKeyDown("e"))
+		{
+			choice=1;
+		}
+		if (Input.GetKeyDown("d"))
+		{
+			choice=2;
+		}
+		if (Input.GetKeyDown("c"))
+		{
+			choice=3;
+		}
 
 
 		if (Input.GetKeyDown("e") || Input.GetKeyDown("d") || Input.GetKeyDown("c"))
@@ -28,41 +137,56 @@ public class optionalpaths : MonoBehaviour {
 		}
 		if(path==1)
 		{
+			if(choice==1)
+			{
 			red.SetActive(true);
 			blue.SetActive(false);
 			green.SetActive(false);
 			map.SetActive(false);
 			Time.timeScale=1;
-			if(Time.timeSinceLevelLoad>5)
+
+			if(Time.timeSinceLevelLoad>3)
 			{
 				camera01.enabled = false;
+				pause = 1;
+
+			}
+
+			}
+			if(choice==2)
+			{
+				red.SetActive(false);
+				blue.SetActive(false);
+				green.SetActive(true);
+				map.SetActive(false);
+				Time.timeScale=1;
+
+				if(Time.timeSinceLevelLoad>3)
+				{
+					camera01.enabled = false;
+					pause = 1;
+				}
+
+			}
+			if(choice==3)
+			{
+				red.SetActive(false);
+				green.SetActive(false);
+				blue.SetActive(true);
+				map.SetActive(false);
+				Time.timeScale=1;
+
+				if(Time.timeSinceLevelLoad>3)
+				{
+					camera01.enabled = false;
+					pause = 1;
+					
+				}
+
 			}
 
 		}
-		if (path==1)
-		{
-			red.SetActive(false);
-			blue.SetActive(false);
-			green.SetActive(true);
-			map.SetActive(false);
-			Time.timeScale=1;
-			if(Time.timeSinceLevelLoad>5)
-			{
-				camera01.enabled = false;
-			}
-		}
-		if (path==1)
-		{
-			red.SetActive(false);
-			green.SetActive(false);
-			blue.SetActive(true);
-			map.SetActive(false);
-			Time.timeScale=1;
-			if(Time.timeSinceLevelLoad>5)
-			{
-				camera01.enabled = false;
-			}
-		}
+
 		/* if (Input.GetKeyUp("w"))
 		{
 			red.SetActive(false);
@@ -88,4 +212,5 @@ public class optionalpaths : MonoBehaviour {
 		}
 
 	}
+
 }
